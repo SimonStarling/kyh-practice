bc5bc2c7c82fefc25955c79072b3926ac849965d
SimonStarling <70509328+SimonStarling@users.noreply.github.com>
2020-08-31 11:28:22 +0200

    Add files via upload

---
+++ Ovning_1.py
@@ -0,0 +1,5 @@
+print("Hej! Jag heter Simon och jag är uppväxt i Gråbo, Lerums kommun.\n")
+print("Jag har tidigare läst vid Högskolan i Borås. Där har jag läst ett antal programmeringskurser med C# som "
+      "programmeringsspråk.\n Har även kört lite SQL och HTML + en testkurs\n")
+print("Varför jag har valt att läsa till Programvarutestare är för att jag tycker kvalitetsäkring av programm är "
+      "intressant\n samt en viktig del i utvecklingsarbetet av ny mjukvara!")
