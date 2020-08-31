b6e4b53929b3deec7954de782349cdc053b0a96f
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-08-31 13:56:30 +0200

    Tagit bort text

--- Ovning_1.py
+++ Ovning_1.py
@@ -4,6 +4,4 @@ print("Jag har tidigare läst vid Högskolan i Borås. Där har jag läst ett an
 print("Varför jag har valt att läsa till Programvarutestare är för att jag tycker kvalitetsäkring av programm är "
       "intressant\n samt en viktig del i utvecklingsarbetet av ny mjukvara!")
 print("Testa för att se om synken fungerar")
-print("New Change")
-print("Will this work?")
-print("Changes from giT")
+print("Nu testade vi att ta bort massa text")
