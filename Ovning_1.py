9ecebad8d2b8522607d676c8396881f5feb80a47
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-01 10:49:09 +0200

    Guessinggame + utökad+1

--- Ovning_1.py
+++ Ovning_1.py
@@ -2,25 +2,29 @@
 
 import random
 
-
 n = random.randint(1, 100)  # Kommer ge n ett värde slumpat mellan 1 till 100
 print("Jag tänker på ett värde mellan 1 till 100. Kan du gissa vilket?")
 guess_number = 0
 
-while True:
-    text = input("Ditt svar: ")
-    as_number = int(text)
 
-    if as_number == n:
-        print("Korrekt! Ditt antal gissningar var " + str(guess_number))
-        guess_number += 1
-        break
+def mainloop(guess_number):
 
-    if as_number < n:
-        print("Fel, värdet är högre... Försök igen!")
-        guess_number += 1
-        print("Antal gissningar: " + str(guess_number))
-    if as_number > n:
-        print("Fel, värdet är mindre.. Försök igen!")
-        guess_number += 1
-        print("Antal gissningar: " + str(guess_number))
+    while True:
+        text = input("Ditt svar: ")
+        as_number = int(text)
+
+        if as_number == n:
+            print("Korrekt! Ditt antal gissningar var " + str(guess_number))
+            guess_number += 1
+            break
+
+        if as_number < n:
+            print("Fel, värdet är högre... Försök igen!")
+            guess_number += 1
+            print("Antal gissningar: " + str(guess_number))
+        if as_number > n:
+            print("Fel, värdet är mindre.. Försök igen!")
+            guess_number += 1
+            print("Antal gissningar: " + str(guess_number))
+
+mainloop(guess_number)
