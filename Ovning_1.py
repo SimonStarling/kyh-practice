9d32848f5330f2f7548e004c17a5dafb6ec853a6
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-01 09:55:30 +0200

    Guessinggame + utökad

--- Ovning_1.py
+++ Ovning_1.py
@@ -1,18 +1,26 @@
+# Program för att gissa ett slumpat nummer mellan 1 - 100.
+
 import random
 
-n = random.randint(1, 20)
-print("I'm thinking of a number between 1 and 20. Guess which?")
+
+n = random.randint(1, 100)  # Kommer ge n ett värde slumpat mellan 1 till 100
+print("Jag tänker på ett värde mellan 1 till 100. Kan du gissa vilket?")
+guess_number = 0
 
 while True:
-    text = input("Your guess: ")
+    text = input("Ditt svar: ")
     as_number = int(text)
 
     if as_number == n:
-        print("Correct!")
+        print("Korrekt! Ditt antal gissningar var " + str(guess_number))
+        guess_number += 1
         break
 
     if as_number < n:
-        print("Wrong, my number is higher... Try again!")
-
+        print("Fel, värdet är högre... Försök igen!")
+        guess_number += 1
+        print("Antal gissningar: " + str(guess_number))
     if as_number > n:
-        print("Wrong, my number is lower... Try again!")
+        print("Fel, värdet är mindre.. Försök igen!")
+        guess_number += 1
+        print("Antal gissningar: " + str(guess_number))
