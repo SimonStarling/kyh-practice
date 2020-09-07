89d2c178de667d9a39fbf03fed9b3ea934778a51
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-07 11:17:18 +0200

    Uppgift 3 klar. Konveterar name till stor bokstav

--- Övning11.py
+++ Övning11.py
@@ -1,13 +1,16 @@
-def is_it_too_long(name): # Kontrolerar längden på namnen som matas in
+def is_it_too_long(name):  # Kontrollerar längden på namnen som matas in
     return len(name) > 5
 
 
 def main():
-    students = input("Ange namn med kommatecken emellan: ").split(",") # Ber användaren skriva in namn och kommatecken
+    students = input("Ange namn med kommatecken emellan: ").split(",")  # Ber användaren skriva in namn och kommatecken
 
+    print(students)
     for name in students:
+
         if is_it_too_long(name):
-            print(f"{name} är för långt!")
+            print(f"{name.title()} är för långt!")
+
 
 if __name__ == '__main__':
     main()
