214b6dfbbd565f49081af7cf1346aeb7e55fbb2a
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-07 11:36:17 +0200

    Uppgift 3 klar. Konveterar name till stor bokstav

--- Övning12.py
+++ Övning12.py
@@ -1,8 +1,13 @@
-def is_it_too_long(name):  # Kontrollerar längden på namnen som matas in
-    return len(name) > 5
+def is_it_too_long(name, max_length):  # Kontrollerar längden på namnen som matas in
+    return len(name) > max_length
 
 
 def main():
+    try:
+        max_length = int(input("Ange maxlängd på namn:"))
+    except ValueError:
+        max_length = 5
+
     students = input("Ange namn med kommatecken emellan: ").split(",")  # Ber användaren skriva in namn och kommatecken
 
     print(students)
