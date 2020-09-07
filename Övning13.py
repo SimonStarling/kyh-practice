7cb410764c8e476e4d6390db7d3fd61234979678
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-07 15:23:40 +0200

    Text från listor väljs randomly och implemeneras i texten. Färdig med slumpen + har med en randint-lösning med!

--- Övning13.py
+++ Övning13.py
@@ -10,7 +10,7 @@ def game():
     kanslor = ["lycka", "glädje", "ilska", "oro", "kärlek", "lust"]
     kanslor_val = random.choice(kanslor)
 
-    land =["Sverige", "Island", "Tyskland", "USA"]
+    land = ["Sverige", "Island", "Tyskland", "USA"]
     land_val = random.choice(land)
 
     pryl = ["bilen", "kylskåpet", "tvättmaskinen", "huset", "grannens hund"]
@@ -19,18 +19,18 @@ def game():
     nationalitet = ["skojare", "pessemister", "skummisar", "kriminella", "rika"]
     nationalitet_val = random.choice(nationalitet)
 
-    namn =["Göran Persson", "Zlatan", "Kurt Wallander", "Martin Beck", "Greta Thunberg"]
+    namn = ["Göran Persson", "Zlatan", "Kurt Wallander", "Martin Beck", "Greta Thunberg"]
     namn_val = random.choice(namn)
 
     jobb = ["vaktmästare", "polischef", "dungeonmaster", "ordförande", "badvakt"]
-    jobb_val = random.choice(jobb)
+    # jobb_val = random.choice(jobb)
 
     plats = ["skolmatsalen", "stranden", "lägenheten", "DND-gruppen", "guildet"]
-    plats_val =random.choice(plats)
+    plats_val = random.choice(plats)
     print(
         f"En app som kan laddas ned till {pryl_val} ska varna {nationalitet_val} som kommit nära någon som smittats med {kanslor_val}.")
     print(
-        f"Jag tycker att ni i {land_val} borde överväga att göra något liknande, säger {namn_val}, {jobb_val} för")
+        f"Jag tycker att ni i {land_val} borde överväga att göra något liknande, säger {namn_val}, {jobb[random.randint(0, 4)]} för")
     print(f"{plats_val}")
 
 
