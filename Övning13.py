607cbd657fc7dde5f3a868d0f026d34b035cbf27
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-07 14:48:26 +0200

    Text från listor väljs randomly och implemeneras i texten.

--- Övning13.py
+++ Övning13.py
@@ -11,9 +11,12 @@ def game():
     kanslor_val = random.choice(kanslor)
     land =["Sverige", "Island", "Tyskland", "USA"]
     land_val = random.choice(land)
-
+    pryl = ["bilen", "kylskåpet", "tvättmaskinen", "huset", "grannens hund"]
+    pryl_val = random.choice(pryl)
+    nationalitet = ["skojare", "pessemister", "skummisar", "kriminella", "rika"]
+    nationalitet_val = random.choice(nationalitet)
     print(
-        f"En app som kan laddas ned till mobiltelefonen ska varna finländare som kommit nära någon som smittats med {kanslor_val}.")
+        f"En app som kan laddas ned till {pryl_val} ska varna {nationalitet_val} som kommit nära någon som smittats med {kanslor_val}.")
     print(
         f"Jag tycker att ni i {land_val} borde överväga att göra något liknande, säger Markku Tervahauta, generaldirektör för")
     print(f"Finska institutet för hälsa och välfärd, THL")
