6f9575573d34bfcd116c196328946ba148da2177
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-08 10:37:07 +0200

    Övning 14 - Klar med uppgift 3+4

--- Övning14.py
+++ Övning14.py
@@ -9,14 +9,16 @@
 
 FRUITS = ['banana', 'apple', 'orange']
 CARS = ['volvo', 'ford', 'tesla']
+COLOR = ["red", "blue", "green", "yellow", "black"]
 
 
 def run():
-    basket = [
-        'volvo', 'is', 'an', 'orange', 'apple'
-    ]
+    basket = ['volvo', 'is', 'red', 'an', 'orange', 'apple', 'black']
     cars = []
+
     fruits = []
+
+    color = []
     rest = []
 
     for item in basket:
@@ -26,19 +28,24 @@ def run():
         elif item in FRUITS:
             fruits.append(item)
 
+        elif item in COLOR:
+            color.append(item)
+
         else:
             rest.append(item)
 
-    write_things(cars, 'Cars')
-    write_things(fruits, 'Fruits')
-    write_things(rest, 'Misc')
+    write_things(sorted(cars), 'Cars')  # Skickar in en sorterad lista sorted(cars) + namnet.
+    write_things(sorted(fruits), 'Fruits')
+    write_things(sorted(color), "Colors")
+    write_things(sorted(rest), 'Misc')
 
 
 def write_things(items, kind):
-    print(f"{kind.upper()} ({len(items)}st)")
+    print(f"{kind.upper()}  ({len(items)}st)")
     for item in items:
         print(f" {item}")
 
 
 if __name__ == '__main__':
+
     run()
