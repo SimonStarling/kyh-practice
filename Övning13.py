20c3fc9f38c73ebf8e7915a9b7068f133bd08e8c
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-08 08:17:14 +0200

    Färdig med Övning 13 - Lagt till kommentarer med förklaringar.

--- Övning13.py
+++ Övning13.py
@@ -3,34 +3,35 @@ En app som kan laddas ned till mobiltelefonen ska varna finländare som kommit n
 – Jag tycker att ni i Sverige borde överväga att göra något liknande, säger Markku Tervahauta, generaldirektör för
 Finska institutet för hälsa och välfärd, THL.
 """
-import random
+import random # Importerar randomfunktionen till programmet.
 
 
 def game():
-    kanslor = ["lycka", "glädje", "ilska", "oro", "kärlek", "lust"]
-    kanslor_val = random.choice(kanslor)
+    kanslor = ["lycka", "glädje", "ilska", "oro", "kärlek", "lust"]  # 1 Variabeln som innehåller elementen i listan
+    kanslor_val = random.choice(kanslor)  # 2 variabel som gör så att ett slumpäsigt ord väljs från listan
 
-    land = ["Sverige", "Island", "Tyskland", "USA"]
-    land_val = random.choice(land)
+    land = ["Sverige", "Island", "Tyskland", "USA"]  # se 1
+    land_val = random.choice(land)  # se 2
 
-    pryl = ["bilen", "kylskåpet", "tvättmaskinen", "huset", "grannens hund"]
-    pryl_val = random.choice(pryl)
+    pryl = ["bilen", "kylskåpet", "tvättmaskinen", "huset", "grannens hund"] # se 1
+    pryl_val = random.choice(pryl)  # se 2
 
-    nationalitet = ["skojare", "pessemister", "skummisar", "kriminella", "rika"]
-    nationalitet_val = random.choice(nationalitet)
+    nationalitet = ["skojare", "pessemister", "skummisar", "kriminella", "rika"] # se 1
+    nationalitet_val = random.choice(nationalitet)  # se 2
 
-    namn = ["Göran Persson", "Zlatan", "Kurt Wallander", "Martin Beck", "Greta Thunberg"]
-    namn_val = random.choice(namn)
+    namn = ["Göran Persson", "Zlatan", "Kurt Wallander", "Martin Beck", "Greta Thunberg"] # se 1
+    namn_val = random.choice(namn)  # se 2
 
-    jobb = ["vaktmästare", "polischef", "dungeonmaster", "ordförande", "badvakt"]
-    # jobb_val = random.choice(jobb)
+    jobb = ["vaktmästare", "polischef", "dungeonmaster", "ordförande", "badvakt"] # Har randintfuntkionen för slumpat val.
+    # jobb_val = random.choice(jobb)  # se 2
 
     plats = ["skolmatsalen", "stranden", "lägenheten", "DND-gruppen", "guildet"]
-    plats_val = random.choice(plats)
-    print(
+    plats_val = random.choice(plats)  # se 2
+
+    print(  # Kommer printa textetn och slumpäsigt välja ord från listorna i varje "f"sträng som kallar på en variabel.
         f"En app som kan laddas ned till {pryl_val} ska varna {nationalitet_val} som kommit nära någon som smittats med {kanslor_val}.")
     print(
-        f"Jag tycker att ni i {land_val} borde överväga att göra något liknande, säger {namn_val}, {jobb[random.randint(0, 4)]} för")
+        f"Jag tycker att ni i {land_val} borde överväga att göra något liknande, säger {namn_val}, {jobb[random.randint(0, 3)]} för")
     print(f"{plats_val}")
 
 
