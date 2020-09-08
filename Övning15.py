c6aeffa488d2e81b3372ea08953d5b924d278b01
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-08 11:53:47 +0200

    Övning 15- Klar med uppgift 1

---
+++ Övning15.py
@@ -0,0 +1,16 @@
+'''15.1 Implementera "wordcount" som jag och Christoffer byggde
+15.2 Ta in en godtycklig text (testa att copy-paste från lorumipsum.com) och skriv ut hur många vokaler som finns i strängen. Tips: "a" in "abcd" är True!
+15.3 Göteborgsvarvet, vilken placering kom XYZ på? Implementera resten av detta program:
+    runners_in_order = “Lisa Lasse Louise Leopold Lova Love Lennart Lena Lisette Linus”.split()
+    vem = input(“Ange löpare du söker placering för'''
+
+def main():
+
+    def wordcount(txt):
+        return len(txt.split())
+
+    text = input("Skriv in text: ")
+    count = wordcount(text)
+    print(f'Antal ord som angivits: {count}')
+
+main()
