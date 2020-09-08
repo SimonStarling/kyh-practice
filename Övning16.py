ec39cd425090697c8cbe31140923da154967a0eb
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-08 14:53:04 +0200

    Övning 16- Kan läsa och få fram rader som innehåller BEAR och X-RAY(ska göra om så vi gör på annat sätt)

--- Övning16.py
+++ Övning16.py
@@ -3,3 +3,24 @@ import pathlib
 p = pathlib.Path('system.log')
 content = p.read_text()
 
+
+def case():
+    dokument = "system.log"
+    important = []
+    keep_phares = ["BEAR", "X-RAY"]
+
+    with open(dokument) as f:
+        f = f.readlines()
+
+    for line in f:
+        line = line.strip()
+        for phrase in keep_phares:
+            if phrase in line:
+                important.append(line)
+                break
+
+    print(*important, sep="\n")
+
+
+if __name__ == '__main__':
+    case()
