39d7495efae7292f4eb567a79106a4e93d89e1e4
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-09 14:09:34 +0200

    Uppgift 2 - Ge input och få Output: Färdig

---
+++ Övning2.py
@@ -0,0 +1,4 @@
+# Skriva in input och printa det tillbaka till användaren
+
+name = input("What is your name?: ") # Ber användaren att skriva in sitt namn och sparar det i variabl name.
+print("Hello " + name) # Kommer att printa meddelande + värdet i variabel name.
