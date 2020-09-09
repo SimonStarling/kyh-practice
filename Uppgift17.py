ed01d56d3644bf7b3fa387d290586a73097f8021
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-09 11:37:48 +0200

    Övning 17: Meny + skriv fil - fixat encoding

--- Uppgift17.py
+++ Uppgift17.py
@@ -1,13 +1,4 @@
 from pathlib import Path
-def alt_1():
-    for line in Path('TODO.txt').read_text().splitlines():
-        print(line)
-
-
-
-
-
-
 
 def main():
     alt = input(f'1. Lista TODO:\n2. Lägg till uppgift:\n3. Ta bort uddgift:\n4. Avbryt programmet:\nVälj alternativ: ')
@@ -17,3 +8,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def alt_1():
+    for line in Path('TODO.txt').read_text(encoding="utf8").splitlines():
+        print(line)
+
+
+alt_1()
