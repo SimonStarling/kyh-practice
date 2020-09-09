a2684f7c8ffa775d5c3125f2f15f6048e3cf0fb6
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-09 10:06:08 +0200

    Övning 16- Fixat så att linjer som innehåller BEAR eller X-RAY printas +  kommentarer

--- Övning16.py
+++ Övning16.py
@@ -1,16 +1,16 @@
-from pathlib import Path
+from pathlib import Path  #  ger oss möjlighet att hämta/läsa filer
 
 
 def case():
-    dokument = "system.log"
-    important = []
-    keep_phares = ["BEAR", "X-RAY"]
-    lines = Path(dokument).read_text().splitlines()
+    dokument = "system.log"             #  Variabel som hanterar filen (system.log)
+    important = []                      #  Lista för att hantera linjerna vi vill ha
+    keep_phares = ["BEAR", "X-RAY"]     #  Orden som vi vill leta efter i Lines
+    lines = Path(dokument).read_text().splitlines()     #  Variabel som hanterar öppning/läsning/stägning av filen system.log
 
     for line in lines:
-        line = line.strip()
-        for phrase in keep_phares:
-            if phrase in line:
+        line = line.strip()             # tar bort Whitespace för varje line
+        for phrase in keep_phares:      #  kommer stega igenom texten och för varje line som har BEAR eller X-RAY.
+            if phrase in line:          #  så kommer listan important öka med varje linje text som matchar IF-statement
                 important.append(line)
                 break
 
