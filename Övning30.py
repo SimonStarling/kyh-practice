276ad7a6da4492370f1e2d9a4affd5cad3ae960a
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-21 12:04:20 +0200

    Övning 30 - Färdig med uppgift 1

--- Övning30.py
+++ Övning30.py
@@ -1,9 +1,8 @@
-
+# 1 1. Bygg ett program som låter användaren mata in ett reg.nr och skriv ut de två grupperna
+# var för sig; använd slicing-syntax för att dela upp inputsträngen. Klar!
 answer = input("Ange Regnummer: ")
 
 bokstäver = answer[0:3]
 siffror = answer[3:]
 
-print(f"{bokstäver}\n{siffror}")
-
-# HEj hej
+print(f"Bokstävsgrupp: {bokstäver}\nSiffergrupp: {siffror}")
