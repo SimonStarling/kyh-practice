8ac757fad77070b6adb21bf716584794e197d7aa
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-21 14:11:01 +0200

    Övning 30 - Färdig med uppgifterna efter ha kollat på annans kod. Skall kommentera och se över.

--- Övning30.py
+++ Övning30.py
@@ -1,8 +1,30 @@
-# 1 1. Bygg ett program som låter användaren mata in ett reg.nr och skriv ut de två grupperna
+# 1 Bygg ett program som låter användaren mata in ett reg.nr och skriv ut de två grupperna
 # var för sig; använd slicing-syntax för att dela upp inputsträngen. Klar!
-answer = input("Ange Regnummer: ")
 
-bokstäver = answer[0:3]
-siffror = answer[3:]
+def reg():
+    answer = input("Ange Regnummer: ")
 
-print(f"Bokstävsgrupp: {bokstäver}\nSiffergrupp: {siffror}")
+    bokstäver = answer[0:3]
+    siffror = answer[3:]
+
+    print(f"Bokstävsgrupp: {bokstäver}\nSiffergrupp: {siffror}")
+
+# 2
+def heltal():
+    nummerlist = input("Ange tal med , imellan: ")
+    str_of_num = nummerlist.split(",")
+    numbers = []
+    for elem in str_of_num:
+        numbers.append(int(elem))
+    print(f"Första talet i listan:{nummerlist[0]}\n Sista talet i listan:{nummerlist[-1]}\nSumma av talen i listan: {sum(numbers)}")
+    print(f"Listan baklänges:{nummerlist[::-1]}")
+
+
+
+
+
+    # print(answer[0])
+    #print(answer[-1])
+if __name__ == '__main__':
+    #reg()
+    heltal()
