8495a988cf6f73a62d19f63156b215fd66195f4f
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-22 10:56:26 +0200

    Övning 33 - Två olika sätt att lösa detta på.

--- Övning32.py
+++ Övning32.py
@@ -1,3 +1,4 @@
+'''
 sträng = input("Skriv text: ")
 ny_sträng = sträng.join('')
 combo_sträng = ny_sträng[::-1]
@@ -8,13 +9,16 @@ if combo_sträng.lower() == ny_sträng.lower():
     print(f"{sträng} är ett Palindrome")
 else:
     print(f"{sträng} är inte ett palindrom")
+'''
 
-'''
-Olof Kod: 
-    lowercase = sträng.lower()
-    lowercase_rev = lowercase[::-1]
-        if lowercase == lowercase_rev:
-             print("Palindrom!)
-        else:
-            print(Ej palindrom!)    
-'''
+#Olof Kod:
+text = input("Skriv text: ")
+lowercase = ''.join([c for c in text.lower() if c != ' '])
+lowercase_rev = lowercase[::-1]
+
+print(len(text.replace(" ","")))
+
+if lowercase == lowercase_rev:
+    print(f"{text} är ett Palindrom!")
+else:
+    print(f"{text} är Ej palindrom!")
