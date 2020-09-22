60a9627dd506398430eccff6c0256c3fe922f399
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-22 10:44:29 +0200

    Övning 33 - Klar

--- Övning32.py
+++ Övning32.py
@@ -8,3 +8,13 @@ if combo_sträng.lower() == ny_sträng.lower():
     print(f"{sträng} är ett Palindrome")
 else:
     print(f"{sträng} är inte ett palindrom")
+
+'''
+Olof Kod: 
+    lowercase = sträng.lower()
+    lowercase_rev = lowercase[::-1]
+        if lowercase == lowercase_rev:
+             print("Palindrom!)
+        else:
+            print(Ej palindrom!)    
+'''
