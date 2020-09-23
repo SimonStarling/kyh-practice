af36f4167a5d984572b78b0e8b82a3a453017450
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-23 09:39:08 +0200

    Övning 33 - Färdig med 33 + 34.1-2

--- Övning33.py
+++ Övning33.py
@@ -1,6 +1,7 @@
 import webbrowser
 from pathlib import Path
-# POCO!
+
+# POCO! Klar med 33 + 34.1-2
 
 OUTPUT_PATH = Path("djur.html")
 TEMPLATE_PATH = Path('djur_template.html')
@@ -12,18 +13,30 @@ class Djur:
         self.carnivore = carnivore
         self.wiki_url = wiki_url
 
+    def carnivore_or_vegetarian(self):
+        if self.carnivore:
+            return "Köttätare"
+        else:
+            return "Vetegarian"
+
 
 if __name__ == '__main__':
     djur = []
-    zebra = Djur('Zebra', False, 'https://sv.wikipedia.org/wiki/Zebror')    # Kallar på init i klassen
+    zebra = Djur('Zebra', False, 'https://sv.wikipedia.org/wiki/Zebror')  # Kallar på init i klassen
     tiger = Djur('Tiger', True, 'https://sv.wikipedia.org/wiki/Tiger')
+    elefant = Djur('Elefant', False, 'https://sv.wikipedia.org/wiki/Elefanter')
+    varg = Djur('Varg', True, 'https://sv.wikipedia.org/wiki/Varg')
+    kattbjörn = Djur('Kattbjörn', False, "https://sv.wikipedia.org/wiki/Kattbj%C3%B6rn")
     djur.append(zebra)
     djur.append(tiger)
+    djur.append(elefant)
+    djur.append(varg)
+    djur.append(kattbjörn)
+
     html = '<html><table>'
+
     for d in djur:
-        cell_2 = 'Vegetarian'
-        if d.carnivore:
-            cell_2 = 'Köttätare'
+        cell_2 = d.carnivore_or_vegetarian()
         html += f'<tr><td><a href="{d.wiki_url}">{d.name}</td><td>{cell_2}</td></tr>\n'
     html += '</table></html>'
     OUTPUT_PATH.write_text(html, encoding='utf8')
