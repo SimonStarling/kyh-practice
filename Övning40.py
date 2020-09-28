6a5350be25b1de004d1e9a2034deb839e5f99800
Simon Netzell <simon.stahrenetzell@student.kyh.se>
2020-09-28 14:47:23 +0200

    Övning 40 - 40.1 färdig

---
+++ Övning40.py
@@ -0,0 +1,22 @@
+
+'''
+# 40.1
+def rev_text(text):
+
+    res = ''
+    for char in text:
+        res = char + res
+    return res
+    y = []
+    for i in text:
+        y.append(i)
+    # print(f"{y[4]}{y[3]}{y[2]}{y[1]}{y[0]}")
+
+
+print(rev_text("ABCDEF"))
+'''
+
+# 40.2
+
+def big_L(text):
+    pass
